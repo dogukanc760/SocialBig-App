dd29d3b58faf88f444e48011ab82bad017f9e8f2
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2021-12-19 21:04:49 +0300

    ne

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -28,6 +28,19 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+router.put("/blabla/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //delete a post
 
 router.delete("/:id", async (req, res) => {
